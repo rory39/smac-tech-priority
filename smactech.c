2dd88d74fd86356ad1c8decf4ee36d5ed0e11d15
rory39 <rory66183@gmail.com>
2026-06-20 22:53:09 -0700

    Update smactech.c

--- smactech.c
+++ smactech.c
@@ -138,28 +138,6 @@ int main() {
 
       if (ptech > -1) {
         if (techlist[ptech].aimil >
-            (techlist[ptech].aicolonize +
-             techlist[ptech].aiinfra +
-             techlist[ptech].aitech) / 3 - 1)
-          aimil++;
-        if (techlist[ptech].aitech >
-            (techlist[ptech].aicolonize +
-             techlist[ptech].aimil +
-             techlist[ptech].aiinfra) / 3 - 1)
-          aitech++;
-        if (techlist[ptech].aiinfra >
-            (techlist[ptech].aicolonize +
-             techlist[ptech].aimil +
-             techlist[ptech].aitech) / 3 - 1)
-          aiinfra++;
-        if (techlist[ptech].aicolonize >
-            (techlist[ptech].aimil +
-             techlist[ptech].aitech +
-             techlist[ptech].aiinfra) / 3 - 1)
-          aicolonize++;
-      }
-      if (ptech > -1) {
-        if (techlist[ptech].aimil >
                  (techlist[ptech].aicolonize +
                   techlist[ptech].aiinfra +
                   techlist[ptech].aitech) / 3)
@@ -184,22 +162,22 @@ int main() {
         if (techlist[stech].aimil >
             (techlist[stech].aicolonize +
              techlist[stech].aiinfra +
-             techlist[stech].aitech) / 3 + 1)
+             techlist[stech].aitech) / 3)
           aimil++;
         if (techlist[stech].aitech >
             (techlist[stech].aicolonize +
              techlist[stech].aimil +
-             techlist[stech].aiinfra) / 3 + 1)
+             techlist[stech].aiinfra) / 3)
           aitech++;
         if (techlist[stech].aiinfra >
             (techlist[stech].aicolonize +
              techlist[stech].aimil +
-             techlist[stech].aitech) / 3 + 1)
+             techlist[stech].aitech) / 3)
           aiinfra++;
         if (techlist[stech].aicolonize >
             (techlist[stech].aimil +
              techlist[stech].aitech +
-             techlist[stech].aiinfra) / 3 + 1)
+             techlist[stech].aiinfra) / 3)
           aicolonize++;
       }
 
